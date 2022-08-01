48f0eb1bb499eeff29cc7a50d8b7f2bb8157c2ac
taogeT <zwtzjd@gmail.com>
2022-08-01 17:50:23 +0800

    feat(auth): 增加静态token检查，简化白名单配置需要输入的格式

--- src/routes/auth/auth.guard.ts
+++ src/routes/auth/auth.guard.ts
@@ -1,24 +1,25 @@
-import { ExecutionContext, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { randomUUID } from 'crypto';
 import { Request } from 'express';
-import * as ipaddr from 'ipaddr.js';
-import { find } from 'lodash';
-import { isIP, isIPv4, isIPv6 } from 'net';
 import { getClientIp } from 'request-ip';
 import { lastValueFrom, Observable } from 'rxjs';
+import { DataSource, Like, Repository } from 'typeorm';
 
-import { ENV_SECRET, INJECT_MODULE_OPTIONS, IS_PUBLIC_KEY, ModuleOptions } from './auth.type';
+import { SiteConfiguration } from '@model/client.entity';
+
+import { ENV_SECRET, IS_PUBLIC_KEY, TOKEN_HEADER } from './auth.type';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(
-    private readonly reflector: Reflector,
-    private readonly configSrv: ConfigService,
-    @Inject(INJECT_MODULE_OPTIONS) private readonly options: ModuleOptions
-  ) {
+  private readonly LOCALHOST_LIST: string[] = ['127.0.0.1', '::1', '0:0:0:0:0:0:0:1'];
+  private readonly siteRepo: Repository<SiteConfiguration>;
+
+  constructor(private readonly reflector: Reflector, private readonly configSrv: ConfigService, conn: DataSource) {
     super();
+    this.siteRepo = conn.getRepository(SiteConfiguration);
   }
 
   async canActivate(context: ExecutionContext) {
@@ -30,7 +31,8 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
       return true;
     }
 
-    // 对token进行检查
+    const req: Request = context.switchToHttp().getRequest();
+    // 1. 动态token检查
     if (this.configSrv.get<string>(ENV_SECRET)) {
       const authProcess = super.canActivate(context);
       try {
@@ -39,40 +41,31 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
             ? authProcess
             : await (authProcess instanceof Observable ? lastValueFrom(authProcess) : authProcess)
         ) {
+          req.user = { ...req.user, traceId: randomUUID() };
           return true;
         }
       } catch (err) {}
     }
-
-    // 当IP属于白名单的情况下，仅支持查询操作
-    const req: Request = context.switchToHttp().getRequest();
-    let currentIp = getClientIp(req);
-    // 排除IPv6情况下的补充地址
-    if (currentIp.startsWith('::ffff:')) {
-      currentIp = currentIp.slice(7);
+    // 2. 静态token检查
+    const token = req.header(TOKEN_HEADER); // 请求头部token
+    let site: SiteConfiguration;
+    if (token) {
+      site = await this.siteRepo.findOneBy({ staticToken: token });
     }
-    const currentAddr = ipaddr.parse(currentIp);
-    const site = find(this.options.sites ?? [], site => {
-      for (const ip of site.whitelist || []) {
-        let w = ip;
-        // 转为submask格式
-        if (isIP(ip)) {
-          if (isIPv4(ip)) {
-            w += '/32';
-          } else if (isIPv6(ip)) {
-            w += '/128';
-          }
-        }
-        // 进行分析比较
-        const [range, bits] = ipaddr.parseCIDR(w);
-        if (currentAddr.kind() === range.kind() && currentAddr.match(range, bits)) {
-          return true;
-        }
+    // 3.白名单IP检查。localhost访问时采用sites中第一个
+    if (!site) {
+      let currentIp = getClientIp(req); // 访问IP
+      if (currentIp.startsWith('::ffff:')) {
+        currentIp = currentIp.slice(7); // 排除IPv6情况下的补充地址
       }
-      return false;
-    });
+      site = await this.siteRepo.findOneBy(
+        this.LOCALHOST_LIST.includes(currentIp) ? {} : { whitelist: Like(`%"${currentIp}"%`) }
+      );
+    }
+
     if (site) {
-      req.user = { clientId: site.clientId };
+      // 仅支持查询操作
+      req.user = { clientId: site.clientId, canTransfer: false, traceId: randomUUID() };
       return true;
     }
 
